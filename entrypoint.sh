345c8bf2bd7a24b8c4ad8e3ed7992a33f342963e
Grant McConnaughey <grantmcconnaughey@gmail.com>
2020-01-05 20:51:00 -0600

    Allow args to be passed to flake8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-args="."
-if [[ $# -ne 0 ]]; then
-    args="$@"
-fi
-flake8 . | lintly --api-key $INPUT_TOKEN --fail-on $INPUT_FAILIF --log --no-post-status
+
+flake8 $* | lintly --api-key $INPUT_TOKEN --fail-on $INPUT_FAILIF --log --no-post-status
